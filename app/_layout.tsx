629960b5d873f75c95629c42f5d93b8ccd23c3e9
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-04-21 03:03:20 +0800

    feat: implement routing and navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,26 @@
 import '@/app/global.css'
 import { Stack } from 'expo-router'
-
+import { Fragment } from 'react'
+import { StatusBar } from 'react-native'
 export default function RootLayout() {
-  return <Stack />
+  return (
+    <Fragment>
+      <StatusBar hidden={true} />
+
+      <Stack>
+        <Stack.Screen
+          name='(tabs)'
+          options={{
+            headerShown: false
+          }}
+        />
+        <Stack.Screen
+          name='movie/[id]'
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack>
+    </Fragment>
+  )
 }
